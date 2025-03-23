8c8d94457de317997346b6701282d43102dd154b
Sergey Ivanenko <storm06rk@gmail.com>
2025-03-23 20:48:50 +0300

    task/001 Task001Test solved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "ru.easycode.zerotoheroandroidtdd"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ru.easycode.zerotoheroandroidtdd"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
