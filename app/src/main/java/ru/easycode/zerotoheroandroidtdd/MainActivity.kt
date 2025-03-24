a93f07e9c26cd2bb3652aeca349f326fd67a4db8
Sergey Ivanenko <storm06rk@gmail.com>
2025-03-24 22:42:29 +0300

    task/007 keep text solved by saveInstanceState

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -12,11 +12,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val textView = findViewById<TextView>(R.id.titleTextView)
-
+        if (savedInstanceState != null) {
+            textView.text = savedInstanceState.getString("androidText")
+        }
 
         val button = findViewById<Button>(R.id.changeButton)
         button.setOnClickListener {
             textView.text = getString(R.string.android_developer_text)
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString("androidText", getString(R.string.android_developer_text))
+        super.onSaveInstanceState(outState)
     }
 }
