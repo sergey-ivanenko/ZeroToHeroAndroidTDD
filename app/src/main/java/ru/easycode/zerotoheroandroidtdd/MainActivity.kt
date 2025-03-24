0b5f4c156bd8b6bbc234e1a9aa9550c8c83dab19
Sergey Ivanenko <storm06rk@gmail.com>
2025-03-24 21:18:54 +0300

    task/006 change text runtime solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,20 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.titleTextView)
+        val changeButton = findViewById<Button>(R.id.changeButton)
+
+        changeButton.setOnClickListener {
+            textView.text = getString(R.string.android_developer_text)
+        }
     }
 }
