924a6a5bf6556b9309876ada36dcff32ef7904e9
Sergey Ivanenko <storm06rk@gmail.com>
2025-04-01 15:06:38 +0300

    task/014 progress solved added some changes

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -8,29 +8,24 @@ import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.Runnable
 
 class MainActivity : AppCompatActivity() {
-
-    private var progressBar: ProgressBar? = null
-    private var titleTextView: TextView? = null
-    private var actionButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        progressBar = findViewById(R.id.progressBar)
-        titleTextView = findViewById(R.id.titleTextView)
-        actionButton = findViewById(R.id.actionButton)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val titleTextView = findViewById<TextView>(R.id.titleTextView)
+        val actionButton = findViewById<Button>(R.id.actionButton)
 
         actionButton?.setOnClickListener {
             titleTextView?.visibility = View.INVISIBLE
             it.isEnabled = false
-            progressBar?.visibility = View.VISIBLE
-            Handler(Looper.getMainLooper()).postDelayed(Runnable {
-                titleTextView?.visibility = View.VISIBLE
-                progressBar?.visibility = View.INVISIBLE
+            progressBar.visibility = View.VISIBLE
+            actionButton.postDelayed({
+                titleTextView.visibility = View.VISIBLE
+                progressBar.visibility = View.INVISIBLE
                 it.isEnabled = true
             }, 3500)
         }
