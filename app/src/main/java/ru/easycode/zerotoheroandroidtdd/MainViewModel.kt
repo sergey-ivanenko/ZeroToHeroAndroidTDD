87061531219bd185b23331de49c8c38a4abc804c
Sergey Ivanenko <storm06rk@gmail.com>
2025-04-03 22:00:40 +0300

    task/016 progress mvvm recreate another solution solved a little change

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainViewModel.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainViewModel.kt
@@ -1,14 +1,13 @@
 package ru.easycode.zerotoheroandroidtdd
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 
 class MainViewModel(private val liveDataWrapper: LiveDataWrapper,
-                    private val repository: Repository) : ViewModel(), ProvideLiveData {
+                    private val repository: Repository) : ProvideLiveData {
 
     private val viewModelScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
 
