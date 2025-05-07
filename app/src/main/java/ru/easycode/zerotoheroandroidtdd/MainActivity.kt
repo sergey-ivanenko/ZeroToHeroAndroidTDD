330690846579110c746dc0c7fe83fc1bee02897b
Sergey Ivanenko <storm06rk@gmail.com>
2025-05-07 23:29:29 +0300

    task/020 input simple solution solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,40 +1,23 @@
 package ru.easycode.zerotoheroandroidtdd
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.textfield.TextInputEditText
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var viewModel: MainViewModel? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val titleTextView = findViewById<TextView>(R.id.titleTextView)
-        val inputEditText = findViewById<TextInputEditText>(R.id.inputEditText)
-        val actionButton = findViewById<Button>(R.id.actionButton)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        viewModel = (application as App).viewModel
-        viewModel?.liveData()?.observe(this) { uiState ->
-            uiState.apply(titleTextView, inputEditText)
+        binding.apply{
+            actionButton.setOnClickListener {
+                val text = inputEditText.text.toString()
+                titleTextView.text = text
+                inputEditText.setText("")
+            }
         }
-
-        actionButton.setOnClickListener {
-            viewModel?.load(inputEditText.text.toString())
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        viewModel?.save(BundleWrapper.Base(outState))
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        viewModel?.restore(BundleWrapper.Base(savedInstanceState))
     }
 }
