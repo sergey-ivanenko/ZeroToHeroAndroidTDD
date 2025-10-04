7b8d9db75d4ced5e8938edd4ce2c24f726b44162
Sergey Ivanenko <storm06rk@gmail.com>
2025-10-04 11:42:34 +0300

    task/021 watch input solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,24 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.actionButton.setOnClickListener {
+            binding.titleTextView.text = binding.inputEditText.text
+            binding.inputEditText.setText("")
+        }
+
+        binding.inputEditText.addTextChangedListener {
+            binding.actionButton.isEnabled = binding.inputEditText.text.toString().length >= 3
+        }
     }
 }
